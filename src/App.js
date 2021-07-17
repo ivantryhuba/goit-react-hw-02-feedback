7a850db14cec5373fbe0f862993d5c818b30bbee
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-07-17 18:51:57 +0300

    Finish logic and render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import { FeedbackBtn } from "./components/FeedbackBtn/FeedbackBtn";
 import { Notification } from "./components/Notification/Notification";
@@ -7,29 +6,54 @@ import { Section } from "./components/Section/Section";
 import { Statistics } from "./components/Statistics/Statistics";
 
 class App extends React.Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  increment = (event) => {
+    this.setState((prevState) => {
+      const { target } = event;
+      const value = target.textContent.toLowerCase();
+      return {
+        [value]: prevState[value] + 1,
+      };
+    });
   };
 
-  increment(evt) {
-    this.setState();
-  }
+  totalCountFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
+  };
+
+  positivePercentage = (good, neutral, bad) => {
+    let total = good + neutral + bad;
+    let percentage = ((good / total) * 100).toFixed(0);
+    return percentage;
+  };
 
   render() {
+    const { increment, totalCountFeedback, positivePercentage } = this;
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
+        <h1>Feedback Service</h1>
         <Section title={"Please leave your feedback"}>
           <FeedbackBtn
             options={["good", "neutral", "bad"]}
-            onClick={this.increment}
+            onLeaveFeedback={increment}
           />
         </Section>
 
         <Section title={"Statistics"}>
-          <Statistics />
-          <Notification text={"No feedback given"} />
+          {totalCountFeedback(good, neutral, bad) < 1 ? (
+            <Notification text={"No feedback given"} />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalCountFeedback(good, neutral, bad)}
+              positivePercentage={positivePercentage(good, neutral, bad)}
+            />
+          )}
         </Section>
       </>
     );
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -24,5 +24,5 @@ Statistics.propTypes = {
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
 };
